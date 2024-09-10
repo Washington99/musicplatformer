b6e95436f698557bb8ceb878eec5658cea496dbd
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-09-10 19:23:47 +0800

    Added music inputs and visual feedback
    Player executes a note on button press. No actually sound play yet.

---
+++ Scripts/music_node.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+###################################################
+## should make this a State?
+###################################################
+
+var animation_playing: bool = false
+
+func _draw() -> void:
+	if animation_playing:
+		draw_circle(Vector2(0,0), 50, $Area2D/CollisionShape2D.debug_color)
+
+func _process(_delta: float) -> void:
+	
+	if Input.is_action_just_pressed("play_note") and !animation_playing:
+		queue_redraw()
+		$Sprite2D.show()
+		$Timer.start()
+		
+		animation_playing = true
+
+func _on_timer_timeout() -> void:
+	$Sprite2D.hide()
+	queue_redraw()
+	animation_playing = false
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,9 +4,12 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
+const animation_time: float = 0.80
+
 # Should probably move the movement itself to another function
 # So add-ons to the player is functional
 func _physics_process(delta: float) -> void:
+	
 	# Gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -29,3 +32,33 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+func resolve_music_key () -> void:
+	#var animation_finished: bool = true
+	#
+	#if !animation_finished:
+		#$MusicNote.position = Vector2(0, 0)
+	
+	if Input.is_action_just_pressed("play_note"):
+		$Area2D.draw_collision_shape()
+		$Sprite2D.visible = true
+		
+		#$MusicNote.visible = true
+		#$MusicNote.position = Vector2(0, 0)
+		## $MusicNote.Colo = Color8(0,255,255)
+		#var tween: Tween = get_tree().create_tween()
+		#tween.tween_property(
+			#$MusicNote, "position", 
+			#$MusicNote.position + Vector2(0, -30), 
+			#0.3
+		#)
+		#await tween.finished
+		#tween.stop()
+		
+		
+	
+	
+			
+
+	
