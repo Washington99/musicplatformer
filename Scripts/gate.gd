55a3e19f375bb35a55e18291940f557f24c9f1e1
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-12-01 15:58:27 +0800

    Gate and Switch
    Fixed gate and switch functionality to correctly behave under multiple connections to each other

--- Scripts/gate.gd
+++ Scripts/gate.gd
@@ -1,5 +1,27 @@
 extends Node2D
 
+@export var needed_vertices: Array[Switch]
+@export var note_hint: Lifeform
+@export var is_ordered: bool = false
+
+func _process(delta: float) -> void:
+	for vertex in needed_vertices:
+		
+		## Assume needed_vertices are all triggered
+		var all_vertices_opened: bool = true
+		
+		## if any vertex wasn't triggered by the correct key then close Gate
+		if not (vertex.switch_name in note_hint.song_list):
+			all_vertices_opened = false
+			close_door()
+			break
+			
+		if all_vertices_opened:
+			open_door()
+		
+		
+		
+
 func open_door() -> void:
 	$TileMapLayer.set_cell(Vector2i(0,0), 0, Vector2i(12,10))
 	$TileMapLayer.set_cell(Vector2i(0,1), 0, Vector2i(12,11))
--- Scripts/switch.gd
+++ Scripts/switch.gd
@@ -4,16 +4,21 @@ class_name Switch
 
 @export var is_one_time: bool = false
 @export var switch_name: String = ""
-@export var trigger_key: String = "0"
+@export var trigger_keys: Array[String] = []
 
 var triggered: bool = false
 signal switch_on(name: String)
 
 func _on_area_entered(area: Area2D) -> void:
-	if triggered:
+	var music_resolver: MusicResolver = area.get_parent()
+	var music_played: String = music_resolver.instrument_played + "_" + str(music_resolver.note_played)
+
+	if music_played in trigger_keys:
+		# triggered = true
 		$TileMapLayer.set_cell(Vector2i(0,0), 0, Vector2i(13,9))
-		$CollisionShape2D.set_deferred("disabled", true)
-		switch_on.emit(switch_name)
+		# $CollisionShape2D.set_deferred("disabled", true)
+		switch_name = music_played
+		# switch_on.emit(switch_name)
 		
 		if !is_one_time:
 			$Timer.start()
@@ -22,11 +27,3 @@ func _on_timer_timeout() -> void:
 	$TileMapLayer.set_cell(Vector2i(0,0), 0, Vector2i(12,9))
 	$CollisionShape2D.set_deferred("disabled", false)
 	triggered = false
-	
-func switch_reset() -> void:
-	$TileMapLayer.set_cell(Vector2i(0,0), 0, Vector2i(12,9))
-	$CollisionShape2D.set_deferred("disabled", false)
-
-func _on_music_node_note_played(note_name: String) -> void:
-	if note_name == trigger_key or trigger_key == "0":
-		triggered = true
