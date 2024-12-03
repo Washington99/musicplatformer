92aec30384e68fe0c5452f327caa23686af1cef4
Ian <ianrafael.aragoza@gmail.com>
2024-12-03 09:19:12 +0800

    updated assets, changed tilemaps, and modified animation logic

--- Scripts/lifeform.gd
+++ Scripts/lifeform.gd
@@ -3,6 +3,7 @@ extends Area2D
 class_name Lifeform
 
 @export var song_list: Array[String]
+@onready var animated_sprite = $AnimatedSprite2D
 
 #func _on_body_entered(body: Node2D) -> void:
 	#if Input.is_key_pressed(KEY_SPACE):
@@ -13,8 +14,10 @@ class_name Lifeform
 			#$AudioStreamPlayer2D.play()
 			#await $AudioStreamPlayer2D.finished
 
+func _ready():
+	animated_sprite.play("default")
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(area: Area2D) -> void:\
 	for audio in song_list:
 		var music_player = "res://Assets/Audio/" + audio + ".wav"
 			
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,13 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 const animation_time: float = 0.80
+var isSinging = false
+var footstep_frames : Array = [4,9]
+
+@onready var music_resolver = get_node("MusicResolver")
+@onready var animated_sprite_2d = get_node("AnimatedSprite2D")
+@onready var jump = $Jump
+@onready var walk = $Walk
 
 # Should probably move the movement itself to another function
 # So add-ons to the player is functional
@@ -20,19 +27,29 @@ func _physics_process(delta: float) -> void:
 
 	# Jump State
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		$AnimatedSprite2D.play("Jump")
 		velocity.y = JUMP_VELOCITY
+		jump.play()
 
 	# Running State
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
-		$AnimatedSprite2D.scale.x = direction
-		$AnimatedSprite2D.play("Run")
+		$AnimatedSprite2D.scale.x = abs($AnimatedSprite2D.scale.x)*direction
+		if abs(velocity.y) < 0.25:
+			$AnimatedSprite2D.play("Run")
+			if !walk.is_playing:
+				print("Walking")
+		else:
+			$AnimatedSprite2D.play("Jump")
 		velocity.x = direction * SPEED
 		
 	# Idle State
 	else:
-		$AnimatedSprite2D.play("Idle")
+		if abs(velocity.y) > 0.25:
+			$AnimatedSprite2D.play("Jump")
+		elif music_resolver.is_playing_sound():
+			$AnimatedSprite2D.play("Sing")
+		else:
+			$AnimatedSprite2D.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if Input.is_action_just_pressed("play_note") or Input.is_action_just_pressed("play_instrument"):
@@ -40,3 +57,12 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
+
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if animated_sprite_2d.animation == "Idle": return
+	if animated_sprite_2d.animation == "Jump": return
+	if animated_sprite_2d.animation == "Sing": return
+	if animated_sprite_2d.frame in footstep_frames:
+		walk.pitch_scale = randf_range(0.8, 1.2)
+		walk.play()
