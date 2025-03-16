ea88fc6726f94b9b2f195549acb4404875bc39d7
iyamiks <95075738+iyamiks@users.noreply.github.com>
2025-03-16 20:44:52 +0800

    Renamed MessageBox and added script for level_tutorial

--- Scenes/level_tutorial.gd
+++ Scenes/level_tutorial.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+@onready var keyboard: Camera2D = $player/OnscreenKeyboard
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	disable_keyboard()
 
+
+func disable_keyboard():
+	if keyboard:
+		keyboard.current = false
+
+func enable_keyboard():
+	if keyboard:
+		keyboard.current = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
